136016dd169a137c2c998ac19c81470f21135def
nateoverd12 <nathan.pousset@club.fr>
2019-03-24 12:27:33 +0100

    settle automatic pictures call => change html code & js code

--- galerie.js
+++ galerie.js
@@ -1,3 +1,12 @@
+// Beginning of automatic pictures call
+let images="";
+for(let i=1;i<9;i++){
+  images+=`<img src="thumbGallery/img${i}.jpg" alt="mosaique">`
+}
+const container=document.getElementById('container-images');
+container.innerHTML=images;
+// End of automatic pictures call
+
 const current = document.getElementById("current");
 const imgs = document.querySelectorAll(".imgs img");
 const opacity = 0.6;
@@ -7,25 +16,17 @@ imgs.forEach(img => img.addEventListener("click", imgClick));
 imgs[0].style.opacity = opacity;
 
 function imgClick(e) {
-// reste the opacity 
+    // reste the opacity 
     imgs.forEach(img => (img.style.opacity = 1 ));
-//change current image to src of clicked image//
+    //change current image to src of clicked image//
     current.src = e.target.src
-
-
-//add fade in class
-current.classList.add("fade-in");
-
-// Remove fade-in class after 0.5s
-setTimeout(() => current.classList.remove("fade-in"),500);
-
-
-
-// change the opacity to opacity var//
+    //add fade in class
+    current.classList.add("fade-in");
+    // Remove fade-in class after 0.5s
+    setTimeout(() => current.classList.remove("fade-in"),500);
+    // change the opacity to opacity var//
     e.target.style.opacity = opacity;
 }   
-
-
 
 /***TOPBUTTON***/
 window.onscroll = function() {scrollFunction();};
