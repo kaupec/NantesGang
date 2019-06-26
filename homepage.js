a0585b3f7c4b37946ff2aa89e04ffff6633f9c4f
nateoverd12 <nathan.pousset@club.fr>
2019-06-26 11:13:28 +0200

    intro animation include

--- homepage.js
+++ homepage.js
@@ -15,3 +15,25 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+/* Jawn animation */
+$(function() {
+  let welcomeSection = $('.welcome-section'),
+      enterButton = welcomeSection.find('.enter-button'),
+      body=document.getElementsByTagName('body');
+
+      setTimeout(function() {
+          welcomeSection.removeClass('content-hidden');
+      },800);
+
+      enterButton.on('click', function(e) {
+          e.preventDefault();
+          welcomeSection.addClass('content-hidden').fadeOut();
+          body.style.background='none';
+      });
+});
+
+document.getElementsByClassName("enter-button")[0].addEventListener('click',()=>{
+  document.getElementsByClassName('welcome-section')[0].style.display='none';
+  document.getElementsByClassName('wrapper')[0].style.display='block';
+})
